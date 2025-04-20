aa5775a75857016c810e6e90da912fe8f8bc6529
Gothamathero98 <gothamacisco223333@gmail.com>
2025-04-20 10:51:39 +0530

    added a comment

--- main.py
+++ main.py
@@ -1,77 +1,16 @@
-# from openai import OpenAI
-# import os
-#
-# api_key = os.getenv("API_KEY")
-#
-# if not api_key:
-#     raise ValueError ("API KEY not found")
-#
-# client = OpenAI(
-#     base_url="https://openrouter.ai/api/v1",
-#     api_key= api_key
-# )
-#
-# chat_history = []
-#
-# personas = {
-#     "default": "you are a help full AI assistant",
-#     "sarcastic": "you are a sarcastic AI who gives witty and mocking responses",
-#     "poet": "you are a poetic AI that responds in rhymes and verses"
-# }
-#
-# print("choose a persona : (default / sarcastic / poet)")
-#
-# user_persona_input = input("enter persona :").strip().lower()
-#
-# persona = personas.get(user_persona_input)
-#
-#
-# chat_history.append({
-#     "role": "system",
-#     "content": personas[user_persona_input]
-# })
-# while True:
-#     user_input = input("enter your prompt :  ")
-#
-#     if user_input == "clear":
-#         chat_history = []
-#         chat_history.append({"role":"system","content":personas[user_persona_input]})
-#
-#         print('chat history cleared')
-#         continue
-#
-#     chat_history.append({
-#         "role": "user",
-#         "content": user_input
-#     })
-#     if user_input == "exit":
-#         break
-#     completion = client.chat.completions.create(
-#
-#         model="qwen/qwen2.5-coder-7b-instruct",
-#         messages=chat_history
-#     )
-#     response = completion.choices[0].message.content
-#     print(response)
-#
-#     chat_history.append({
-#         "role": "user",
-#         "content": response
-#     })
-
-
 from openai import OpenAI
-
 import os
+
+#Implemented an AI chatbot using openrouter
 
 api_key = os.getenv("API_KEY")
 
 if not api_key:
-    raise ValueError("Api key not found")
+    raise ValueError ("API KEY not found")
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key = api_key
+    api_key= api_key
 )
 
 chat_history = []
@@ -88,6 +27,7 @@ user_persona_input = input("enter persona :").strip().lower()
 
 persona = personas.get(user_persona_input)
 
+
 chat_history.append({
     "role": "system",
     "content": personas[user_persona_input]
@@ -95,9 +35,9 @@ chat_history.append({
 while True:
     user_input = input("enter your prompt :  ")
 
-    if user_input == 'clear':
+    if user_input == "clear":
         chat_history = []
-        chat_history.append({"role": "system", "content": personas[user_persona_input]})
+        chat_history.append({"role":"system","content":personas[user_persona_input]})
 
         print('chat history cleared')
         continue
@@ -120,3 +60,5 @@ while True:
         "role": "user",
         "content": response
     })
+
+
